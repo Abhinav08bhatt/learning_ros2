374157a9c80d6ef2747000e588d9bfbf545451e9
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2026-08-21 17:50:16 +0530

    action completed (unpacked) (god bless me)

--- fibonacci_action_client.py
+++ fibonacci_action_client.py
@@ -26,9 +26,12 @@ class FibonacciActionClient(Node):
             'fibonacci'
         )
 
-# the function
+# the start of chain
     def send_goal(self, order):
-
+        """
+        this function returns a FUTURE OBJECT (empty right now)
+        - it represent a promise that someday the server will tell us : GOAL ACCEPTED (or not)
+        """
     # making a FEEDBACK OBJECT from the Feedback.action file
         goal_msg = Fibonacci.Goal()
     # giving it the argument of the function
@@ -37,26 +40,64 @@ class FibonacciActionClient(Node):
     # waiting for the server to be available
         self.action_client.wait_for_server()
 
-        self.send_goal_future = self.action_client.send_goal_async(goal_msg)
+    # making the future object (empty rn but with promise) (when we dont have to see the feedback)
+        # self.send_goal_future = self.action_client.send_goal_async(goal_msg) 
+
+    # making the future object (empty rn but with promise) but also telling that if we get ANY FEEDBACK we will callback the function
+        self.send_goal_future = self.action_client.send_goal_async(goal_msg, feedback_callback = self.feedback_callback)
+
+    # add_done_callback tells ROS : the moment the future gets a value -> jump to callback function
         self.send_goal_future.add_done_callback(self.goal_response_callback)
 
+# once the send_goal function got the future value meaning : SERVER GOT THE JOB LETTER AND IT REPLIED : accepted or rejected
     def goal_response_callback(self,future):
 
+    # looking the response of the server
         goal_handle = future.result()
+
+    # if the server said rejected we display it else we continue
         if not goal_handle.accepted:
             self.get_logger().info("Goal rejected :(")
 
+    # if we get here our request was accepted from the server
         self.get_logger().info("Goal accepted :)")
+        """
+        the server has accepted the request but have'nt produced an output yet...
+        so we make another FUTURE OBJECT (empty right now)
+        - it represent a promise that someday the server will tell us : JOB COMPLETED (or failed)
+        """
 
+    # making the FUTURE OBJECT (empty rn)
         self.get_result_future = goal_handle.get_result_async()
+
+    # using add_done_callback so that once server tells us i completed the job we can continue to next callback
         self.get_result_future.add_done_callback(self.get_result_callback)
 
+# when the server tells us i am done with the job here is the result , we call this function (it got result info in it)
     def get_result_callback(self,future):
 
-        result = future.result().result
+    # looking at the result the server sent
+        # result = future.result().result   <- tutorial line (but we can simply)
+    # the server sent the result in a truck... the FUTURE OBJECT is the truck (we open it)
+    # inside that truck is the result wrapped in a box... the `result()` is that box (we open it)
+        result_in_box = future.result()
+    # when we open the wrapped box...inside we find our RESULT that the server sent for us
+        result = result_in_box.result
+
+    # we log the result
         self.get_logger().info(f"Result {result.sequence}")
+
+    # we terminate everything once work is done
         rclpy.shutdown()
 
+# in-case we get any feedback from the server we trigger this function :
+    def feedback_callback(self, feedback_msg):
+
+    # we open the feedback message
+        feedback = feedback_msg.feedback
+
+    # we display the feedback the server sent us
+        self.get_logger().info(f'Received feedback: {feedback.partial_sequence}')
 
 # code starts here
 def main(args=None):
